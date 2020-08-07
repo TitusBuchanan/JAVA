ebe4fd0499828003f5cf68b142f7e64a57dca6d8
titus buchanan <titusbuchananjr@gmail.com>
2020-08-06 21:28:49 -0400

    added a variable and printed variable out

--- hello.java
+++ hello.java
@@ -2,5 +2,8 @@ public class hello {
 
     public static void main(String[] args) {
         System.out.println("Hello Titus");
+
+        int myFirstNumber = 3;
+        System.out.println(myFirstNumber);
     }
 }
