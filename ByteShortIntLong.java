13e2087a4921784269330d25790be08b36eb994d
titus buchanan <titusbuchananjr@gmail.com>
2020-08-15 15:22:08 -0400

    added multiple primtive type's

--- ByteShortIntLong.java
+++ ByteShortIntLong.java
@@ -12,6 +12,27 @@ public class ByteShortIntLong {
         System.out.println("Busted Min Value = " + (myMinIntValue - 1));
 
         int myMaxIntTest = 2147483647;
+
+        byte myMinByteValue = Byte.MIN_VALUE;
+        byte myMaxByteValue = Byte.MAX_VALUE;
+        System.out.println("Byte minimum Value + " + myMinByteValue);
+        System.out.println("Byte Maximum Value + " + myMaxByteValue);
+
+        short myMinShortValue = Short.MIN_VALUE;
+        short myMaxShortValue = Short.MAX_VALUE;
+        System.out.println("Short minimum Value + " + myMinShortValue);
+        System.out.println("Short Maximum Value + " + myMaxShortValue);
+
+        long myLongValue = 100L;
+        long myMinLongValue = Long.MIN_VALUE;
+        long myMaxLongValue = Long.MAX_VALUE;
+        System.out.println("Long minimum Value + " + myMinLongValue);
+        System.out.println("Long Maximum Value + " + myMaxLongValue);
+        long bigLongLiteralValue = 21474836488L;
+        System.out.println(bigLongLiteralValue);
+
+        short bigShortLiteralValue = 32767;
+
     }
 
 }
